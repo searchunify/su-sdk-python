bf817cf82b6989eba840ebb9732c17c3ed7f9f35
Mohan Rana <mohanr@grazitti.com>
2022-10-18 13:12:34 +0530

    added packages in setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,24 @@ import setuptools
 setuptools.setup(
     include_package_data=True,
     name="searchunify",
-    version='1.0.2',
+    version='1.0.3',
     description='The SearchUnify SDK enables developers to easily work with the SearchUnify platform and build scalable solutions with search, analytics, crawlers and more.',
     author='SearchUnify',
     author_email='ankur.mahajan@grazitti.com',
     url='https://www.searchunify.com/',
     packages=[
         'searchunify',
-        'searchunify.core', 
+        'searchunify.core',
+        'searchunify.core.analytics',
+        'searchunify.core.auth',
+        'searchunify.core.content',
+        'searchunify.core.search',
         'searchunify.client', 
-        'searchunify.utils', 
+        'searchunify.utils',
+        'searchunify.utils.apis',
+        'searchunify.utils.constants',
+        'searchunify.utils.http_client',
+        'searchunify.utils.validation'
     ],
     install_requires=['requests==2.23.0', 'schema==0.7.4']
 )
